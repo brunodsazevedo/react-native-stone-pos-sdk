2de19989403397a4cd52bd69d417a531e9b0de10
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2024-02-09 11:08:44 -0300

    feat: tailwind config in example

--- example/babel.config.js
+++ example/babel.config.js
@@ -13,5 +13,6 @@ module.exports = {
         },
       },
     ],
+    'nativewind/babel',
   ],
 };
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
 import { multiply } from 'react-native-stone-pos-sdk';
 
 export default function App() {
@@ -11,21 +11,8 @@ export default function App() {
   }, []);
 
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center">
       <Text>Result: {result}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
